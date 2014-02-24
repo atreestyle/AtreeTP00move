a4e0d62d693cc7c84948ac7e1036b58b0cb48c2b
atreestyle <atreestyle@gmail.com>
2014-02-24 21:56:04 +0800

    FIX TEST

--- src/com/example/atreetp00/MainActivity.java
+++ src/com/example/atreetp00/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.atreetp00;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-
+/* test */
 public class MainActivity extends Activity {
 
 	@Override
